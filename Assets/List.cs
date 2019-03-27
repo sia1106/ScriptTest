3ec0896051e432d2410b268b8d1a813e030a5c01
Inai <inai067@live.com>
2019-03-28 05:13:37 +0900

    Tech Academy Unity Course Lesson4課題
    通常の課題と発展課題のスクリプトを書きました

---
+++ Assets/List.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List : MonoBehaviour {
+    
+    void Start() {        
+        //課題：配列を宣言して出力しましょう
+        //条件１：要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化してください
+        int[]array = { 1, 2, 3, 4, 5};
+        int num = array.Length; //要素数を取得しておく
+        int temp;  //インデックスを指定する変数
+
+        //条件２：for文を使い、配列の各要素の値を順番に表示してください
+        Debug.Log("全ての要素を順番に表示します");
+        for (int i = 0; i < num; i++) {
+            Debug.Log(array[i]);
+        }
+
+        //条件３：for文を使い、配列の各要素の値を逆順に表示してください
+        //要素を追加しても対応できるようにした
+        Debug.Log("逆順に表示します");
+        for (int i=0; i < num; i++) {       
+            temp = num -1 -i; //要素数ｰ1(最後尾)から始めて1ずつ減っていく
+            Debug.Log(array[temp]);
+        }
+        Debug.Log("おわり");
+    }
+    	
+}
---
+++ Assets/UseMagic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+    //発展課題：Bossクラスに、変数mpとMagic関数を作成してStart関数から呼び出しましょう
+    //int型の変数mpを宣言し、53で初期化してください
+    private int mp = 53;
+    private int mplost = 5;//消費MP
+
+    //mpを消費して魔法攻撃をするMagic関数を作ってください
+    //Magic関数内でmpを5減らし、コンソールに「魔法攻撃をした。残りMPは〇〇。」と表示してください。
+    //mpが足りない場合、「MPが足りないため魔法が使えない。」と表示してください
+
+    public void Magic()
+    {
+        if (mp >= mplost) //現在MPが消費MP以上ならば成功
+        {
+            this.mp -= mplost;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        } else {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+}
+
+public class UseMagic : MonoBehaviour
+{
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        //Magic関数を呼び出して、魔法を使ってください
+        //Magic関数を10回使った後に、更にMagic関数を呼び出すと、mpが足りないメッセージが表示されることを確認してください
+
+        for (int i = 1; i <= 11; i++) {
+            Debug.Log("-----" + i + "回目-----"); //見づらいので
+            lastboss.Magic(); //消費MP5の魔法を11回使う
+        }
+    }
+
+}
